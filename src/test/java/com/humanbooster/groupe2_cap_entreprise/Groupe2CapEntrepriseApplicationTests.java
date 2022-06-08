29535d13f3faa1efa86bbbbbd9ce9738651e4650
Augustin Charly <62054313+augustincharly@users.noreply.github.com>
2022-06-08 09:54:09 +0200

    first commit

---
+++ src/test/java/com/humanbooster/groupe2_cap_entreprise/Groupe2CapEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanbooster.groupe2_cap_entreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe2CapEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
