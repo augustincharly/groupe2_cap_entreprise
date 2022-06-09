7bd167e62e12c582bf4782e6f8b6fe0e0d4c1094
Augustin Charly <62054313+augustincharly@users.noreply.github.com>
2022-06-09 15:24:32 +0200

    creation of a controller for avis

---
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/controller/AvisController.java
@@ -0,0 +1,33 @@
+package com.humanbooster.groupe2_cap_entreprise.controller;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.humanbooster.groupe2_cap_entreprise.dto.AvisDTO;
+import com.humanbooster.groupe2_cap_entreprise.service.IAvisService;
+
+@Controller
+@RequestMapping("avis")
+public class AvisController {
+
+	@Autowired
+	private IAvisService avisService;
+
+	@GetMapping
+	public String getAvis(Model model) {
+		// List<AvisDTO> avisDTOs = avisService.getAvisDTOs();
+		List<AvisDTO> avisDTOs = avisService.getAvisDTOs();
+		
+		model.addAttribute("list_avis", avisDTOs);
+		return "avisListe";
+	}
+
+}
