1c432e519a0ef544d07b7977117e05a833dfabd0
Augustin Charly <62054313+augustincharly@users.noreply.github.com>
2022-06-09 15:24:19 +0200

    creation of avisService (interface and implementation)

--- src/main/java/com/humanbooster/groupe2_cap_entreprise/service/AvisService.java
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/service/AvisService.java
@@ -1,12 +1,15 @@
 package com.humanbooster.groupe2_cap_entreprise.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.humanbooster.groupe2_cap_entreprise.dto.AvisDTO;
 import com.humanbooster.groupe2_cap_entreprise.entity.Avis;
 import com.humanbooster.groupe2_cap_entreprise.repository.AvisRepository;
+import com.humanbooster.groupe2_cap_entreprise.transformer.TransformerFactory;
 
 @Service
 public class AvisService {
@@ -17,6 +20,14 @@ public class AvisService {
 	public List<Avis> getAvis() {
 		return avisRepository.findAll();
 	}
-	
+
+	public List<AvisDTO> getAvisDTOs() {
+		List<AvisDTO> avisDTOs = new ArrayList<AvisDTO>();
+		avisRepository.findAll().forEach(avis -> {
+			AvisDTO avisDTO = TransformerFactory.getAvisTransformer().transform(avis);
+			avisDTOs.add(avisDTO);
+		});
+		return avisDTOs;
+	}
 
 }
