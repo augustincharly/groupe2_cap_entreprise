f5b5186a39711baf90833cf32b1b3b38c8239ecd
Augustin Charly <62054313+augustincharly@users.noreply.github.com>
2022-06-10 14:28:55 +0200

    add endpoint in AvisController for details

--- src/main/java/com/humanbooster/groupe2_cap_entreprise/controller/AvisController.java
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/controller/AvisController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.humanbooster.groupe2_cap_entreprise.dto.AvisDTO;
@@ -23,8 +24,17 @@ public class AvisController {
 		List<AvisDTO> avisDTOs = avisService.getAvisDTOs();
 
 		model.addAttribute("list_avis", avisDTOs);
-		
+
 		return "avisListe";
+	}
+
+	@GetMapping("{id}")
+	public String getAvis(@PathVariable(name = "id") Long id, Model model) {
+		AvisDTO avis = avisService.getAvisDTO(id);
+
+		model.addAttribute("avis", avis);
+
+		return "avisDetails";
 	}
 
 }
