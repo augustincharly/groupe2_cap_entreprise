077f946b84fbb702182af22c01efa0aada355d4b
Augustin Charly <62054313+augustincharly@users.noreply.github.com>
2022-06-10 10:20:18 +0200

    create JeuService interface and implementation

--- src/main/java/com/humanbooster/groupe2_cap_entreprise/service/IJeuService.java
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/service/IJeuService.java
@@ -1,9 +1,17 @@
 package com.humanbooster.groupe2_cap_entreprise.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.humanbooster.groupe2_cap_entreprise.dto.JeuDTO;
 import com.humanbooster.groupe2_cap_entreprise.entity.Jeu;
 
+@Component
 public interface IJeuService {
 
-	void save(Jeu jeu);
+	public void save(Jeu jeu);
+
+	public List<JeuDTO> getJeux();
 
 }
