622cda8640d6cb74189efc03b59e92c93a5620e4
Augustin Charly <62054313+augustincharly@users.noreply.github.com>
2022-06-14 12:52:07 +0200

    add function to get a single Moderateur in service and implementation

--- src/main/java/com/humanbooster/groupe2_cap_entreprise/service/ModerateurService.java
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/service/ModerateurService.java
@@ -7,15 +7,19 @@ import com.humanbooster.groupe2_cap_entreprise.entity.Moderateur;
 import com.humanbooster.groupe2_cap_entreprise.repository.ModerateurRepository;
 
 @Component
-public class ModerateurService implements IModerateurService{
+public class ModerateurService implements IModerateurService {
 
-	
 	@Autowired
 	private ModerateurRepository moderateurRepository;
-	
+
 	@Override
 	public void save(Moderateur moderateur) {
 		moderateurRepository.save(moderateur);
+	}
+
+	@Override
+	public Moderateur getModerateur(Long id) {
+		return moderateurRepository.findById(id).get();
 	}
 
 }
