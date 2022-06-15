7689695f4201158b5f094c5229ede70341e30365
manfredCorbeau <manfred03@live.fr>
2022-06-15 14:36:00 +0200

    Create EnvironmentVariable.java
    création variable d'environnement

---
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/configuration/EnvironmentVariable.java
@@ -0,0 +1,10 @@
+package com.humanbooster.groupe2_cap_entreprise.configuration;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvironmentVariable {
+
+	public final static Integer ITEMS_PER_PAGE = 2 ;
+	
+}
