1a0a4a05586dca55778b44cb16e87f44375d11d5
manfredCorbeau <manfred03@live.fr>
2022-06-15 09:23:36 +0200

    Creation login controller & loginSuccessHandler

---
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.humanbooster.groupe2_cap_entreprise.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    
+
+    
+
+}
