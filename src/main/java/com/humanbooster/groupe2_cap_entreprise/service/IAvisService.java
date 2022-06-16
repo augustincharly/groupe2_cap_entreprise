1ba27239386b7b7f89ac009b7a58da62cf0aff93
manfredCorbeau <manfred03@live.fr>
2022-06-16 10:54:51 +0200

    Update IAvisService.java
    ajout import pageable

--- src/main/java/com/humanbooster/groupe2_cap_entreprise/service/IAvisService.java
+++ src/main/java/com/humanbooster/groupe2_cap_entreprise/service/IAvisService.java
@@ -3,6 +3,7 @@ package com.humanbooster.groupe2_cap_entreprise.service;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import com.humanbooster.groupe2_cap_entreprise.dto.AvisDTO;
